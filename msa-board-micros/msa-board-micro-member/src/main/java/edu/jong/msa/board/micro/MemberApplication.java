b9768b875398932cbbe1f2c1250cd191912d24e8
kim-jong-young <kim-jong-young@218.38.137.27>
2023-07-09 21:11:09 +0900

    refactor: add password encode bean

--- msa-board-micros/msa-board-micro-member/src/main/java/edu/jong/msa/board/micro/MemberApplication.java
+++ msa-board-micros/msa-board-micro-member/src/main/java/edu/jong/msa/board/micro/MemberApplication.java
@@ -2,14 +2,24 @@ package edu.jong.msa.board.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.jong.msa.board.common.BoardConstants.Packages;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = Packages.ROOT_PACKAGE)
 public class MemberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberApplication.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
