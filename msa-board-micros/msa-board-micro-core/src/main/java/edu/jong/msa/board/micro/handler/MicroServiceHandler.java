b5753de732943ab355fdbe50c1daedccd39a8196
kim-jong-young <kim-jong-young@218.38.137.27>
2023-07-09 18:50:00 +0900

    feat: add entity exception handle

--- msa-board-micros/msa-board-micro-core/src/main/java/edu/jong/msa/board/micro/handler/MicroServiceHandler.java
+++ msa-board-micros/msa-board-micro-core/src/main/java/edu/jong/msa/board/micro/handler/MicroServiceHandler.java
@@ -1,5 +1,9 @@
 package edu.jong.msa.board.micro.handler;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +20,29 @@ public class MicroServiceHandler {
 				.body(ErrorResponse.builder()
 						.status(e.getStatus())
 						.messageList(e.getMessageList())
+						.build());
+	} 
+
+	@ExceptionHandler({
+		EntityNotFoundException.class,
+		EntityExistsException.class
+	})
+	ResponseEntity<ErrorResponse> handlerEntityException(Exception e) {
+		
+		HttpStatus status = null;
+		
+		if (e instanceof EntityNotFoundException) {
+			status = HttpStatus.NOT_FOUND;
+		} else if (e instanceof EntityExistsException) {
+			status = HttpStatus.CONFLICT;
+		} else {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return ResponseEntity.status(status)
+				.body(ErrorResponse.builder()
+						.status(status)
+						.message(e.getMessage())
 						.build());
 	} 
 	
