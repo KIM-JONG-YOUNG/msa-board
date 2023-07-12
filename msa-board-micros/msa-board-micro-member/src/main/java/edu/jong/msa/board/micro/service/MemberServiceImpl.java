ea465d0fbcc048b9a52f6404a19e69ee55e498e0
kim-jong-young <kim-jong-young@218.38.137.27>
2023-07-12 23:56:50 +0900

    fix: update login service

--- msa-board-micros/msa-board-micro-member/src/main/java/edu/jong/msa/board/micro/service/MemberServiceImpl.java
+++ msa-board-micros/msa-board-micro-member/src/main/java/edu/jong/msa/board/micro/service/MemberServiceImpl.java
@@ -3,9 +3,6 @@ package edu.jong.msa.board.micro.service;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +13,14 @@ import edu.jong.msa.board.client.request.MemberParam;
 import edu.jong.msa.board.client.response.MemberDetails;
 import edu.jong.msa.board.common.BoardConstants.CachingKeys;
 import edu.jong.msa.board.common.BoardConstants.TopicNames;
+import edu.jong.msa.board.common.type.DBCodeEnum.State;
 import edu.jong.msa.board.common.utils.ObjectMapperUtils;
 import edu.jong.msa.board.domain.entity.MemberEntity;
 import edu.jong.msa.board.domain.repository.MemberEntityRepository;
 import edu.jong.msa.board.kafka.dto.KafkaMessage;
 import edu.jong.msa.board.kafka.producer.KafkaMessageProducer;
+import edu.jong.msa.board.micro.exception.AlreadyExistsDataException;
+import edu.jong.msa.board.micro.exception.NotFoundDataException;
 import edu.jong.msa.board.micro.mapper.MemberEntityMapper;
 import edu.jong.msa.board.redis.service.RedissonLockService;
 import edu.jong.msa.board.web.exception.ParamValidException;
@@ -47,7 +47,7 @@ public class MemberServiceImpl implements MemberService {
 	public UUID createMember(MemberParam param) {
 		
 		if (repository.existsByUsername(param.getUsername())) {
-			throw new EntityExistsException("이미 계정이 존재합니다.");
+			throw new AlreadyExistsDataException("이미 계정이 존재합니다.");
 		} else {
 			String lockKey = CachingKeys.MEMBER_USERNAME_LOCK_KEY + param.getUsername();
 			
@@ -73,7 +73,7 @@ public class MemberServiceImpl implements MemberService {
 	public UUID modifyMember(UUID id, MemberParam param) {
 		
 		MemberEntity entity = repository.findById(id)
-				.orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+				.orElseThrow(() -> new NotFoundDataException("존재하지 않는 회원입니다."));
 
 		String lockKey = CachingKeys.MEMBER_LOCK_KEY + id;
 
@@ -113,7 +113,7 @@ public class MemberServiceImpl implements MemberService {
 		} catch (Exception e) {
 			
 			MemberDetails details = mapper.toDetails(repository.findById(id)
-					.orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다.")));
+					.orElseThrow(() -> new NotFoundDataException("존재하지 않는 회원입니다.")));
 			
 			String cachingKey = CachingKeys.MEMBER_KEY + details.getId();
 			String cachingVal = ObjectMapperUtils.toJson(details);
@@ -129,12 +129,16 @@ public class MemberServiceImpl implements MemberService {
 	public MemberDetails loginMember(MemberParam param) {
 		
 		MemberEntity entity = repository.findByUsername(param.getUsername())
-				.orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+				.orElseThrow(() -> new ParamValidException("존재하지 않는 회원입니다."));
 		
-		if (encoder.matches(param.getPassword(), entity.getPassword())) {
-			return mapper.toDetails(entity);
+		if (entity.getState() == State.ACTIVE) {
+			if (encoder.matches(param.getPassword(), entity.getPassword())) {
+				return mapper.toDetails(entity);
+			} else {
+				throw new ParamValidException("비밀번호가 일치하지 않습니다.");
+			}
 		} else {
-			throw new ParamValidException("비밀번호가 일치하지 않습니다.");
+			throw new ParamValidException("비활성화된 회원입니다.");
 		}
 	}
 
