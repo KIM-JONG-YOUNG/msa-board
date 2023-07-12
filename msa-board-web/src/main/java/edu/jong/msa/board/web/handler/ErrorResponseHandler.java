ebcf305669d311f48a3fe037e834de7ed1d03494
kim-jong-young <kim-jong-young@218.38.137.27>
2023-07-12 23:54:28 +0900

    refactor: change error handler abstract

--- msa-board-web/src/main/java/edu/jong/msa/board/web/handler/ErrorResponseHandler.java
+++ msa-board-web/src/main/java/edu/jong/msa/board/web/handler/ErrorResponseHandler.java
@@ -1,32 +1,17 @@
 package edu.jong.msa.board.web.handler;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import edu.jong.msa.board.web.exception.ParamValidException;
 import edu.jong.msa.board.web.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestControllerAdvice
-@Order(Integer.MIN_VALUE)
-public class ErrorResponseHandler extends ResponseEntityExceptionHandler {
+public abstract class ErrorResponseHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(ParamValidException.class)
-	protected ResponseEntity<Object> handleParamValidException(ParamValidException e) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(ErrorResponse.builder()
-						.status(HttpStatus.BAD_REQUEST)
-						.messageList(e.getMessageList())
-						.build());
-	}
-	
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
