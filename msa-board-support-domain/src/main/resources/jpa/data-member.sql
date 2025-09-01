59a945afb6f97bc8d7580a7357576ef6f0255ad5
Kim Jong Young <jykim@saltlux.com>
2025-09-01 16:54:11 +0900

    [feat] 데이터 초기화 스크립트 추가

---
+++ msa-board-support-domain/src/main/resources/jpa/data-member.sql
@@ -0,0 +1,12 @@
+INSERT INTO `tb_member`
+    (`id`, `member_username`, `member_password`, `member_name`, `member_gender`, `member_email`, `member_group`, `created_date_time`, `updated_date_time`, `state`)
+VALUES (X'bc7d5c3eaf784ed09a208fe87ed9107a', 'username-0', 'password-0', 'name-0', 'M', 'email-0@email.com', 1, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'f54f79537de3493b8631a6d92a81dfa1', 'username-1', 'password-1', 'name-1', 'F', 'email-1@email.com', 2, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'40ef296d866949688f441b5b44f574ed', 'username-2', 'password-2', 'name-2', 'M', 'email-2@email.com', 1, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'96b0956bd3f043329fe52aa38be10811', 'username-3', 'password-3', 'name-3', 'F', 'email-3@email.com', 2, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'3acca93c80b04c84a83cc87e32bd6708', 'username-4', 'password-4', 'name-4', 'M', 'email-4@email.com', 1, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'e8f527082cf24ad0bb07890a208545a3', 'username-5', 'password-5', 'name-5', 'F', 'email-5@email.com', 2, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'4404a5feb1e249d8b6db5aabef52bd23', 'username-6', 'password-6', 'name-6', 'M', 'email-6@email.com', 1, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'ae3d018e0adb4c12a5d7d511bd59260c', 'username-7', 'password-7', 'name-7', 'F', 'email-7@email.com', 2, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'24a273b2a68840d78831a9bf09ab7606', 'username-8', 'password-8', 'name-8', 'M', 'email-8@email.com', 1, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1),
+       (X'6fdf7fea739244ba9bf530899fceb8d9', 'username-9', 'password-9', 'name-9', 'F', 'email-9@email.com', 2, '2025-09-01 15:52:41', '2025-09-01 15:52:41', 1);
---
+++ msa-board-support-domain/src/test/java/com/jong/msa/board/support/domain/DomainDataTests.java
@@ -0,0 +1,248 @@
+package com.jong.msa.board.support.domain;
+
+import com.jong.msa.board.common.constants.DateTimeFormats;
+import com.jong.msa.board.common.enums.Gender;
+import com.jong.msa.board.common.enums.Group;
+import com.jong.msa.board.common.enums.State;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lombok.Builder;
+import org.junit.jupiter.api.Test;
+
+public class DomainDataTests {
+
+    private Random random = new Random();
+    private List<MemberRecord> memberRecords;
+    private List<PostRecord> postRecords;
+    private List<CommentRecord> commentRecords;
+
+    public static String uuidToHex(UUID uuid) {
+        // Most Significant Bits (MSB)와 Least Significant Bits (LSB) 추출
+        long msb = uuid.getMostSignificantBits();
+        long lsb = uuid.getLeastSignificantBits();
+        // MSB와 LSB를 16진수 문자열로 변환하고 결합
+//        return String.format("%016x%016x", msb, lsb);
+        String hexString = String.format("%016x%016x", msb, lsb);
+        // BINARY(16) 형식으로 반환
+        return "X'" + hexString + "'";
+    }
+
+    @Test
+    void createDataSql() {
+
+        memberRecords = IntStream.range(0, 10)
+            .mapToObj(i -> MemberRecord.builder()
+                .id(UUID.randomUUID())
+                .username("username-" + i)
+                .password("password-" + i)
+                .name("name-" + i)
+                .gender(i % 2 == 0 ? Gender.MALE : Gender.FEMALE)
+                .email("email-" + i + "@email.com")
+                .group(i % 2 == 0 ? Group.ADMIN : Group.USER)
+                .createdDateTime(LocalDateTime.now())
+                .updatedDateTime(LocalDateTime.now())
+                .state(State.ACTIVE)
+                .build())
+            .toList();
+
+        postRecords = memberRecords.stream()
+            .flatMap(member -> IntStream.range(0, 10)
+                .mapToObj(i -> PostRecord.builder()
+                    .id(UUID.randomUUID())
+                    .title(String.format("Post Title %d by %s", i, member.username()))
+                    .content(String.format("Post Content %d by %s", i, member.username()))
+                    .writerId(member.id())
+                    .views(1)
+                    .createdDateTime(LocalDateTime.now())
+                    .updatedDateTime(LocalDateTime.now())
+                    .state(State.ACTIVE)
+                    .build()))
+            .toList();
+
+        commentRecords = new ArrayList<>();
+        commentRecords.addAll(postRecords.stream()
+            .flatMap(post -> IntStream.range(0, 3)
+                .mapToObj(i -> {
+                    MemberRecord member = memberRecords.get(random.nextInt(memberRecords.size() - 1));
+                    return CommentRecord.builder()
+                        .id(UUID.randomUUID())
+                        .content(String.format("Comment Content %d by %s", i, member.username()))
+                        .writerId(member.id())
+                        .postId(post.id())
+                        .parentId(null)
+                        .createdDateTime(LocalDateTime.now())
+                        .updatedDateTime(LocalDateTime.now())
+                        .state(State.ACTIVE)
+                        .build();
+                }))
+            .toList());
+
+        commentRecords.addAll(commentRecords.stream()
+            .map(comment -> {
+                MemberRecord member = memberRecords.get(random.nextInt(memberRecords.size() - 1));
+                return CommentRecord.builder()
+                    .id(UUID.randomUUID())
+                    .content(String.format("Child Comment Content by %s", member.username()))
+                    .writerId(member.id())
+                    .postId(comment.postId())
+                    .parentId(comment.id())
+                    .createdDateTime(LocalDateTime.now())
+                    .updatedDateTime(LocalDateTime.now())
+                    .state(State.ACTIVE)
+                    .build();
+            })
+            .toList());
+
+        commentRecords.addAll(commentRecords.stream()
+            .map(comment -> {
+                MemberRecord member = memberRecords.get(random.nextInt(memberRecords.size() - 1));
+                return CommentRecord.builder()
+                    .id(UUID.randomUUID())
+                    .content(String.format("Child Comment Content by %s", member.username()))
+                    .writerId(member.id())
+                    .postId(comment.postId())
+                    .parentId(comment.id())
+                    .createdDateTime(LocalDateTime.now())
+                    .updatedDateTime(LocalDateTime.now())
+                    .state(State.ACTIVE)
+                    .build();
+            })
+            .toList());
+
+        String memberDataQueryPrefix = """
+            INSERT INTO `tb_member` 
+                (`id`,
+                `member_username`,
+                `member_password`,
+                `member_name`,
+                `member_gender`,
+                `member_email`,
+                `member_group`,
+                `created_date_time`,
+                `updated_date_time`,
+                `state`
+            ) VALUES  
+            """;
+        String postDataQueryPrefix = """
+            INSERT INTO `tb_post` (
+                `id`,
+                `post_title`,
+                `post_content`,
+                `post_writer_id`,
+                `post_views`,
+                `created_date_time`,
+                `updated_date_time`,
+                `state`
+            ) VALUES  
+            """;
+        String commentDataQueryPrefix = """
+            INSERT INTO `tb_comment` (
+                `id`,
+                `comment_content`,
+                `comment_writer_id`,
+                `comment_post_id`,
+                `comment_parent_id`,
+                `created_date_time`,
+                `updated_date_time`,
+                `state`
+            ) VALUES  
+            """;
+
+        String memberDataQuery = memberRecords.stream()
+            .map(MemberRecord::toInsertQuery)
+            .collect(Collectors.joining(",\n", memberDataQueryPrefix, ";"));
+
+        String postDataQuery = postRecords.stream()
+            .map(PostRecord::toInsertQuery)
+            .collect(Collectors.joining(",\n", postDataQueryPrefix, ";"));
+
+        String commentDataQuery = commentRecords.stream()
+            .map(CommentRecord::toInsertQuery)
+            .collect(Collectors.joining(",\n", commentDataQueryPrefix, ";"));
+
+        System.out.println(memberDataQuery);
+        System.out.println(postDataQuery);
+        System.out.println(commentDataQuery);
+    }
+
+    @Builder
+    public record MemberRecord(
+        UUID id,
+        String username,
+        String password,
+        String name,
+        Gender gender,
+        String email,
+        Group group,
+        LocalDateTime createdDateTime,
+        LocalDateTime updatedDateTime,
+        State state
+    ) {
+
+        public String toInsertQuery() {
+            String genderString = gender == Gender.MALE ? "M" : "F";
+            int groupCode = group == Group.ADMIN ? 1 : 2;
+            int stateCode = state == State.ACTIVE ? 1 : 0;
+            String createdDateTimeString = createdDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            String updatedDateTimeString = updatedDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            return String.format("(%s, '%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', %d)",
+                uuidToHex(id), username, password, name, genderString, email, groupCode,
+                createdDateTimeString, updatedDateTimeString, stateCode);
+        }
+    }
+
+    @Builder
+    public record PostRecord(
+        UUID id,
+        String title,
+        String content,
+        UUID writerId,
+        int views,
+        LocalDateTime createdDateTime,
+        LocalDateTime updatedDateTime,
+        State state
+    ) {
+
+        public String toInsertQuery() {
+            int stateCode = state == State.ACTIVE ? 1 : 0;
+            String createdDateTimeString = createdDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            String updatedDateTimeString = updatedDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            return String.format("(%s, '%s', '%s', %s, %d, '%s', '%s', %d)",
+                uuidToHex(id), title, content, uuidToHex(writerId), views,
+                createdDateTimeString, updatedDateTimeString, stateCode);
+        }
+    }
+
+    @Builder
+    public record CommentRecord(
+        UUID id,
+        String content,
+        UUID writerId,
+        UUID postId,
+        UUID parentId,
+        LocalDateTime createdDateTime,
+        LocalDateTime updatedDateTime,
+        State state
+    ) {
+
+        public String toInsertQuery() {
+            int stateCode = state == State.ACTIVE ? 1 : 0;
+            String createdDateTimeString = createdDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            String updatedDateTimeString = updatedDateTime.format(DateTimeFormats.DATE_TIME_FORMATTER);
+            if (parentId == null) {
+                return String.format("(%s, '%s', %s, %s, NULL, '%s', '%s', %d)",
+                    uuidToHex(id), content, uuidToHex(writerId), uuidToHex(postId),
+                    createdDateTimeString, updatedDateTimeString, stateCode);
+            }
+            return String.format("(%s, '%s', %s, %s, %s, '%s', '%s', %d)",
+                uuidToHex(id), content, uuidToHex(writerId), uuidToHex(postId), uuidToHex(parentId),
+                createdDateTimeString, updatedDateTimeString, stateCode);
+        }
+    }
+
+}
