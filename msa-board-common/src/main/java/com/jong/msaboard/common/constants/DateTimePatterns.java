1ec12d4f5d4d997018d1742dba0c2d738ac19fef
KIM JONG YOUNG <46014387+KIM-JONG-YOUNG@users.noreply.github.com>
2026-06-06 23:38:35 +0900

    feat: 날짜 형식 상수 추가

---
+++ msa-board-common/src/main/java/com/jong/msaboard/common/constants/DateTimePatterns.java
@@ -0,0 +1,9 @@
+package com.jong.msaboard.common.constants;
+
+public final class DateTimePatterns {
+
+    public static final String TIME = "HH:mm:ss";
+    public static final String DATE = "yyyy-MM-dd";
+    public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+}
